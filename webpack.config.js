712cfb7519b2d890aba6c8554156451a2819da73
Saas Labs <bivasbsht4@gmail.com>
2019-08-30 01:05:46 +0530

    final_push

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    entry: "./src/index.js",
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    }
+};
